e46503db04c89a7590ae3c5c6f44b5e0632fc206
USER-PC10 <mrshishikina@gmail.com>
2024-03-22 16:59:47 +0300

    first commit

--- libApiBot.py
+++ libApiBot.py
@@ -1,5 +1,6 @@
 import telebot
 import time
+from pytube import YouTube
 
 token = '7174359547:AAHrqaYoQfm0LixkAD0t383a8gqVAUZQZTQ'
 
@@ -26,6 +27,22 @@ def send_sticker(message):
     file_ID = 'CAACAgIAAxkBAAMqZfRmWTR40D0nWWJfoYAcypVZvIkAAjAAA97wgS5ZST-D8V71KjQE'
     bot.send_sticker(message.chat.id, file_ID)
 
+@bot.message_handler(commands=['vidio'])
+def vidio(message):
+    bot.reply_to(message, 'Вставь ссылку на видио')
+    bot.register_next_step_handler(message,vidios_url)
+
+def save_vidios(message, vidio_url):
+    save_location = '/vidios'
+    yt = YouTube(vidios_url)
+    stream = yt.streams.filter(res='360p', progressive=True).first()
+    stream.download(output_path=save_location)
+
+@bot.message_handler(content_types=['text'])
+def vidios_url(message):
+    vidio_url = message.text
+    return vidio_url
+    save_vidios()
 
 
 
